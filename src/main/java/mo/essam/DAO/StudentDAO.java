df658dc215e18cf32bbffbac25655cbf0433f33c
Mohamed Essam <ana3ssam2017@gmail.com>
2019-10-21 15:50:23 +0200

    Using RestController without create any controller just connect between model and repository that from JpaRepository type to can using any Http Methods without writing any implementation

--- src/main/java/mo/essam/DAO/StudentDAO.java
+++ src/main/java/mo/essam/DAO/StudentDAO.java
@@ -4,15 +4,18 @@ import mo.essam.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "student" ,path = "student" )
 public interface StudentDAO extends JpaRepository<Student,Integer> {
 
-    List<Student> findByName(String name);
-
-    List<Student> findByIdGreaterThan(int id);
-
-    @Query("from Student where name = ?1 order by email")
-    List<Student>  findByNameSorted(String name);
+//    List<Student> findByName(String name);
+//
+//    List<Student> findByIdGreaterThan(int id);
+//
+//    @Query("from Student where name = ?1 order by email")
+//    List<Student>  findByNameSorted(String name);
 }
--- src/main/java/mo/essam/controllers/StudentController.java
+++ src/main/java/mo/essam/controllers/StudentController.java
@@ -1,102 +1,102 @@
-package mo.essam.controllers;
-
-import mo.essam.DAO.StudentDAO;
-import mo.essam.models.Student;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
-import java.util.Optional;
-
-@Controller
-@RequestMapping("/student")
-public class StudentController {
-
-    @Autowired
-    private StudentDAO studentDAO;
-
-    @RequestMapping("/addform")
-    public ModelAndView studentForm(){
-        ModelAndView view = new ModelAndView();
-        view.setViewName("studentlogin");
-        return view;
-    }
-
-    @RequestMapping("/add")
-    public void addStudent(Student student){
-        studentDAO.save(student);
-    }
-
-    @RequestMapping("/show")
-    public ModelAndView getStudent(@RequestParam int id){
-        ModelAndView view = new ModelAndView("home");
-        Student student = studentDAO.findById(id).orElse(null);
-        view.addObject("student",student);
-        return view;
-    }
-
-
-    @RequestMapping("/delete")
-    public ModelAndView deleteStudent(@RequestParam int id){
-        String response;
-        ModelAndView view = new ModelAndView("home");
-        Student student = studentDAO.findById(id).orElse(null);
-        if (student == null)
-            response = "Error this id is not found";
-        else {
-            response = "we are delete this id = " + id;
-            studentDAO.deleteById(id);
-        }
-        view.addObject("response", response);
-        return view;
-    }
-
-
-    @RequestMapping("/update")
-    public ModelAndView updateStudent(@RequestParam int id,Student student){
-        ModelAndView view = new ModelAndView("home");
-        Student Orignalstudent = studentDAO.findById(id).orElse(null);
-        if (Orignalstudent == null)
-           studentDAO.save(student);
-        else {
-            student.setId(id);
-            studentDAO.save(student);
-        }
-        view.addObject("student", student);
-        return view;
-    }
-
-
-    @RequestMapping("/showgeaterthen")
-    public ModelAndView getStudentbyidGeaterthan(@RequestParam int id){
-        ModelAndView view = new ModelAndView("home");
-        List<Student> students = studentDAO.findByIdGreaterThan(id);
-        System.out.println(students);
-        return view;
-    }
-
-    @RequestMapping("/showname")
-    public ModelAndView getStudentbyname(@RequestParam String name){
-        ModelAndView view = new ModelAndView("home");
-        List<Student> students = studentDAO.findByName(name);
-        System.out.println(students);
-        view.addObject("student",students);
-        return view;
-    }
-
-    @RequestMapping("/showsorted")
-    public ModelAndView getStudentSortedbyname(@RequestParam String name){
-        ModelAndView view = new ModelAndView("home");
-        List<Student> students = studentDAO.findByNameSorted(name);
-        System.out.println(students);
-        view.addObject("student",students);
-        return view;
-    }
-
-
-}
+//package mo.essam.controllers;
+//
+//import mo.essam.DAO.StudentDAO;
+//import mo.essam.models.Student;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Controller
+//@RequestMapping("/student")
+//public class StudentController {
+//
+//    @Autowired
+//    private StudentDAO studentDAO;
+//
+//    @RequestMapping("/addform")
+//    public ModelAndView studentForm(){
+//        ModelAndView view = new ModelAndView();
+//        view.setViewName("studentlogin");
+//        return view;
+//    }
+//
+//    @RequestMapping("/add")
+//    public void addStudent(Student student){
+//        studentDAO.save(student);
+//    }
+//
+//    @RequestMapping("/show")
+//    public ModelAndView getStudent(@RequestParam int id){
+//        ModelAndView view = new ModelAndView("home");
+//        Student student = studentDAO.findById(id).orElse(null);
+//        view.addObject("student",student);
+//        return view;
+//    }
+//
+//
+//    @RequestMapping("/delete")
+//    public ModelAndView deleteStudent(@RequestParam int id){
+//        String response;
+//        ModelAndView view = new ModelAndView("home");
+//        Student student = studentDAO.findById(id).orElse(null);
+//        if (student == null)
+//            response = "Error this id is not found";
+//        else {
+//            response = "we are delete this id = " + id;
+//            studentDAO.deleteById(id);
+//        }
+//        view.addObject("response", response);
+//        return view;
+//    }
+//
+//
+//    @RequestMapping("/update")
+//    public ModelAndView updateStudent(@RequestParam int id,Student student){
+//        ModelAndView view = new ModelAndView("home");
+//        Student Orignalstudent = studentDAO.findById(id).orElse(null);
+//        if (Orignalstudent == null)
+//           studentDAO.save(student);
+//        else {
+//            student.setId(id);
+//            studentDAO.save(student);
+//        }
+//        view.addObject("student", student);
+//        return view;
+//    }
+//
+//
+//    @RequestMapping("/showgeaterthen")
+//    public ModelAndView getStudentbyidGeaterthan(@RequestParam int id){
+//        ModelAndView view = new ModelAndView("home");
+//        List<Student> students = studentDAO.findByIdGreaterThan(id);
+//        System.out.println(students);
+//        return view;
+//    }
+//
+//    @RequestMapping("/showname")
+//    public ModelAndView getStudentbyname(@RequestParam String name){
+//        ModelAndView view = new ModelAndView("home");
+//        List<Student> students = studentDAO.findByName(name);
+//        System.out.println(students);
+//        view.addObject("student",students);
+//        return view;
+//    }
+//
+//    @RequestMapping("/showsorted")
+//    public ModelAndView getStudentSortedbyname(@RequestParam String name){
+//        ModelAndView view = new ModelAndView("home");
+//        List<Student> students = studentDAO.findByNameSorted(name);
+//        System.out.println(students);
+//        view.addObject("student",students);
+//        return view;
+//    }
+//
+//
+//}
