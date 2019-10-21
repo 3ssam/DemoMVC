bd3726241bafa88e6171da6f0d78d04c88e5ac32
Mohamed Essam <ana3ssam2017@gmail.com>
2019-10-21 11:44:45 +0200

    Make model as entity to create table for this model in h2 database

--- src/main/java/mo/essam/models/Student.java
+++ src/main/java/mo/essam/models/Student.java
@@ -1,6 +1,14 @@
 package mo.essam.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private int age;
