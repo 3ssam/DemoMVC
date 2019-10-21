556dd7bf245574b3ada6c23cc586a88aed52c225
Mohamed Essam <ana3ssam2017@gmail.com>
2019-10-21 11:56:41 +0200

    create file contain data sql that is execute in runtime

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into Student values (100,22,'mohamed@yahoo.com','mohamed');
+insert into Student values (200,33,'essam@yahoo.com','essam');
+insert into Student values (300,44,'ahmed@yahoo.com','ahmed');
+insert into Student values (400,24,'abdo@yahoo.com','abdo');
+insert into Student values (500,28,'karim@yahoo.com','karim');
+insert into Student values (600,30,'micheal@yahoo.com','micheal');
+insert into Student values (700,40,'jhon@yahoo.com','jhon');
