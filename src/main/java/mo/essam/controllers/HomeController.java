8123781578236785df0daf4b218cdda10cda692b
Mohamed Essam <ana3ssam2017@gmail.com>
2019-10-21 10:39:36 +0200

    Move view in folder pages to more safe then Add some Configuration in application.properties file to can know the new place of views and give the extension to these files

--- src/main/java/mo/essam/controllers/HomeController.java
+++ src/main/java/mo/essam/controllers/HomeController.java
@@ -9,6 +9,6 @@ public class HomeController {
     @RequestMapping("home")
     public String home(){
         System.out.println("You are ask /home url");
-        return  "home.jsp";
+        return "home";
     }
 }
